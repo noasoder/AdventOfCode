6bff0ed337af9477df3e07dc9f157c6ba6751498
Noa Söderlund <noasoderlund01@gmail.com>
2021-12-07 23:17:00 +0100

    Update main.cpp

--- AdventOfCode7-2/main.cpp
+++ AdventOfCode7-2/main.cpp
@@ -1,8 +1,17 @@
 #include "Program.h"
-#include <cstdio>
+#include <iostream>
+#include <chrono>
 
 int main()
 {
+	using std::chrono::high_resolution_clock;
+	using std::chrono::duration;
+
+	auto t1 = high_resolution_clock::now();
 	Program* p = new Program();
+	auto t2 = high_resolution_clock::now();
+
+	duration<double, std::milli> ms_double = t2 - t1;
+	std::cout << "computations took: " << ms_double.count() << " ms" << std::endl;
 	delete p;
 }
